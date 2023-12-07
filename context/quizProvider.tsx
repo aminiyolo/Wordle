a15fced1eb5e21b010298a5cc2f2003d4c9f8935
aminiyolo <aminiyolo@gmail.com>
2023-12-07 23:36:32 +0900

    feat: QuizProvider에 Keypad check를 위한 값 추가

--- context/quizProvider.tsx
+++ context/quizProvider.tsx
@@ -1,8 +1,18 @@
-import { createContext, ReactNode } from 'react';
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useState,
+} from 'react';
 import { WORDS } from '@/constant/words';
+
+type KeypadCheckType = Record<string, number>;
 
 type QuizContextType = {
   quiz: string[];
+  keypadCheck: KeypadCheckType;
+  setKeypadCheck: Dispatch<SetStateAction<KeypadCheckType>>;
 };
 
 export const QuizContext = createContext<QuizContextType | null>(null);
@@ -12,10 +22,13 @@ export default function QuizProvider({ children }: { children: ReactNode }) {
   const YEAR = TODAY.getFullYear();
   const MONTH = TODAY.getMonth();
   const DAY = TODAY.getDate();
-  const QUIZ = WORDS[YEAR - MONTH * DAY].split('');
+  const quiz = WORDS[YEAR - MONTH * DAY].split('');
+
+  // 1 -> 정답에 포함x, 2 -> 정답에 포함되있지만 다른 위치, 3 -> 정답 및 올바른 위치
+  const [keypadCheck, setKeypadCheck] = useState<KeypadCheckType>({});
 
   return (
-    <QuizContext.Provider value={{ quiz: QUIZ }}>
+    <QuizContext.Provider value={{ quiz, keypadCheck, setKeypadCheck }}>
       {children}
     </QuizContext.Provider>
   );
