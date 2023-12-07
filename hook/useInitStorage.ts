b71b17ff8e507bbd477436d9d782e8f099209f8a
aminiyolo <aminiyolo@gmail.com>
2023-12-08 00:54:58 +0900

    fix: 로컬스토리지 날짜 비교 로직 오류 수정

--- hook/useInitStorage.ts
+++ hook/useInitStorage.ts
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 
-const DATE = new Date();
+const TODAY = new Date();
+const DATE = TODAY.getFullYear() + TODAY.getMonth() + TODAY.getDay();
 
 function initialize() {
   localStorage.setItem(
@@ -8,7 +9,7 @@ function initialize() {
     JSON.stringify({
       currentIdx: 0,
       records: ['', '', '', '', '', ''],
-      date: DATE.getFullYear() + DATE.getMonth() + DATE.getDay(),
+      date: DATE,
     }),
   );
 }
@@ -17,7 +18,7 @@ function initialize() {
 export function useInitStorage() {
   useEffect(() => {
     const { date } = JSON.parse(localStorage.getItem('history') as string);
-    if (!localStorage.getItem('history') || date !== DATE) initialize();
+    if (!date || date !== DATE) initialize();
     // 오늘 날짜의 기록이 아닌 기록이 있거나 기록이 없다면, init
   }, []);
 }
