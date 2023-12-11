9f776b8c2e99795b05228d29e8f4fd57d51cd68b
aminiyolo <aminiyolo@gmail.com>
2023-12-12 01:21:01 +0900

    정답인지를 확인하기 위한 값 QuizProvider에 추가

--- context/quizProvider.tsx
+++ context/quizProvider.tsx
@@ -13,17 +13,21 @@ type QuizContextType = {
   quiz: string[];
   keypadCheck: CheckType;
   check: CheckType;
+  hit: boolean;
   setKeypadCheck: Dispatch<SetStateAction<CheckType>>;
 };
 
 export const QuizContext = createContext<QuizContextType | null>(null);
 
+const TODAY = new Date();
+const YEAR = TODAY.getFullYear();
+const MONTH = TODAY.getMonth();
+const DAY = TODAY.getDate();
+const quiz = WORDS[YEAR - MONTH * DAY].split('');
+
 export default function QuizProvider({ children }: { children: ReactNode }) {
-  const TODAY = new Date();
-  const YEAR = TODAY.getFullYear();
-  const MONTH = TODAY.getMonth();
-  const DAY = TODAY.getDate();
-  const quiz = WORDS[YEAR - MONTH * DAY].split('');
+  const { records } = JSON.parse(localStorage.getItem('history') ?? '{}');
+  const hit = records?.some((record: string) => record === quiz.join(''));
 
   // 1 -> 정답에 포함x, 2 -> 정답에 포함되있지만 다른 위치, 3 -> 정답 및 올바른 위치
   const [keypadCheck, setKeypadCheck] = useState<CheckType>({});
@@ -41,7 +45,7 @@ export default function QuizProvider({ children }: { children: ReactNode }) {
 
   return (
     <QuizContext.Provider
-      value={{ quiz, keypadCheck, setKeypadCheck, check: quizCheck }}
+      value={{ quiz, keypadCheck, setKeypadCheck, hit, check: quizCheck }}
     >
       {children}
     </QuizContext.Provider>
