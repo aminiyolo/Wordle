eafb9d64b5700de774f7a5a4782dbdeea40a38ed
aminiyolo <aminiyolo@gmail.com>
2023-12-11 16:36:07 +0900

    refactor: localStorage.getItem string 타입으로 명시하던 부분 변경

--- hook/useInitStorage.ts
+++ hook/useInitStorage.ts
@@ -17,7 +17,7 @@ function initialize() {
 // localStorage가 비어있는 경우(기록이 없다면) initial 값 세팅
 export function useInitStorage() {
   useEffect(() => {
-    const history = JSON.parse(localStorage.getItem('history') as string);
+    const history = JSON.parse(localStorage.getItem('history') ?? '{}');
     // 오늘 날짜의 기록이 아닌 기록이 있거나 기록이 없다면, init
     if (!history?.date || history?.date !== DATE) initialize();
   }, []);
