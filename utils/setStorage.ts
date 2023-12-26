c977c9fcfa509f4b3a73f17125670eab39e8d325
aminiyolo <aminiyolo@gmail.com>
2023-12-26 15:16:34 +0900

    fix: 일반 함수 내에서 쓰여지는 함수이므로, 커스텀훅 네이밍에서 일반 함수 네이밍으로 변경

--- utils/setStorage.ts
+++ utils/setStorage.ts
@@ -1,5 +1,5 @@
 // localStorage에 새로운 값을 세팅
-export function useSetStorage(key: string, value: number | string[]) {
+export function setStorage(key: string, value: number | string[]) {
   const data = JSON.parse(localStorage.getItem('history') ?? '{}');
   localStorage.setItem('history', JSON.stringify({ ...data, [key]: value }));
 }
