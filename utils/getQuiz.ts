6943e0492c214dda5fa358f057997b1e70835c14
aminiyolo <aminiyolo@gmail.com>
2024-01-03 15:28:21 +0900

    refactor: getQuiz 개별 함수로 분리

---
+++ utils/getQuiz.ts
@@ -0,0 +1,9 @@
+import { WORDS } from '@/constant/words';
+
+export function getQuiz() {
+  const TODAY = new Date();
+  const YEAR = TODAY.getFullYear();
+  const MONTH = TODAY.getMonth() + 1;
+  const DAY = TODAY.getDate();
+  return WORDS[YEAR - MONTH * DAY].split('');
+}
