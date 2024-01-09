69aba6476215329c92342d0cd889f07d72aa4da8
aminiyolo <aminiyolo@gmail.com>
2024-01-09 20:17:03 +0900

    Metatag 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,15 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Aminiyolo Wordle',
   description: '재밌어 보여서 만드는 게임',
+  openGraph: {
+    title: 'Aminiyolo Wordle',
+    description: '재밌어 보여서 만드는 게임',
+  },
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+  },
 };
 
 export default function RootLayout({
